ae4220dd8ac6283da16ba6f73960016a6adbc265
Julien Constant <julienconstant190@gmail.com>
2023-07-03 18:05:48 +0200

    Added missing fields in UserPatchDTO

--- api/users/dto.d.ts
+++ api/users/dto.d.ts
@@ -27,14 +27,46 @@ export interface UserPatchDto extends UserPostDto {
 	/** The user's id */
 	id: number;
 
+	/** The user's email */
+	email: string;
+
+	/** The user's password */
+	password: string;
+
+	/** The user's birthday */
+	birthday: Date;
+
+	/** The user's first name */
+	first_name: string;
+
+	/** The user's last name */
+	last_name: string;
+
 	/** The user's nickname */
 	nickname?: string;
 
 	/** The user's gender */
 	gender?: string;
 
+	/** The user's pronouns */
+	pronouns?: string;
+
+	/** The user's secondary email (for communication) */
+	secondary_email?: string;
+
+	/** The user's phone number */
+	phone?: string;
+
+	/** The user's parent contact (for minors only) */
+	parent_contact?: string;
+
 	/** The user's cursus */
+	// TODO: might be a good idea to create a new entity for this
 	cursus?: string;
+
+	/** The user's specialty at UTBM */
+	// TODO: might be a good idea to create a new entity for this
+	specialty?: string;
 
 	/** The user's promotion */
 	promotion?: number;
